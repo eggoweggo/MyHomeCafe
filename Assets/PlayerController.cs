90c6af6f0d718ea99ebca1f133f69567b9921e27
Shannon B <89764051+eggoweggo@users.noreply.github.com>
2022-07-28 00:39:32 -0600

    basic character movement and some collisions

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 // Takes and handles input and movement for a player character
 public class PlayerController : MonoBehaviour
 {   
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+
     Vector2 movementInput;
     Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +28,17 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate() {
         // If movement input is not zero, try to move
-        if(movementInput != Vector2.ZERO){
+        if(movementInput != Vector2.zero){
+            // Check for potential collisions
+            int count = rb.Cast(
+                movementInput, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus the offset
 
+                if(count == 0){
+                    rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+                }
         }
     }
 
