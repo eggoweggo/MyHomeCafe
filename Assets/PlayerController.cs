7b9673b51e129e4c949b126a3770e13d92d53b03
Shannon B <89764051+eggoweggo@users.noreply.github.com>
2022-07-27 23:26:52 -0600

    saved cafe scene

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Takes and handles input and movement for a player character
 public class PlayerController : MonoBehaviour
-{
+{   
+    Vector2 movementInput;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // // Update is called once per frame
+    // void Update()
+    // {
         
+    // }
+
+    private void FixedUpdate() {
+        // If movement input is not zero, try to move
+        if(movementInput != Vector2.ZERO){
+
+        }
+    }
+
+    void OnMove(InputValue movementValue) {
+        movementInput = movementValue.Get<Vector2>();
     }
 }
