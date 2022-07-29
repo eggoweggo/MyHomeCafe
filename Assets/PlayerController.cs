2daad329a53a95a46902540d0a3eb87c15a14b9c
Shannon B <89764051+eggoweggo@users.noreply.github.com>
2022-07-29 01:25:43 -0600

    changes

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -16,31 +16,28 @@ public class PlayerController : MonoBehaviour
     private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
 
-    private void FixedUpdate() {
+    public void FixedUpdate() {
         // If movement input is not zero, try to move
         if(movementInput != Vector2.zero){
             bool success = TryMove(movementInput);
 
-            if(!success) {
+            if(!success) 
+            {
+                // Try left/right
                 success = TryMove(new Vector2(movementInput.x, 0));
-            }
 
-            if(!success) {
-                success = TryMove( new Vector2(0, movementInput.y));
+                if(!success) 
+                {
+                    success = TryMove( new Vector2(0, movementInput.y));
+                }
             }
-
             animator.SetBool("isMoving", success);
         } else {
             animator.SetBool("isMoving", false);
@@ -48,22 +45,20 @@ public class PlayerController : MonoBehaviour
     }
 
     private bool TryMove(Vector2 direction) {
-        if(direction != Vector2.zero) {
-            // Check for potential collisions
-            int count = rb.Cast(
-                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
-                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
-                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
-                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus the offset
+        // Check for potential collisions
+        int count = rb.Cast(
+            direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+            movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+            castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+            moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus the offset
 
-            if(count == 0){
-                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-                return true;
-            } else {
-                return false;
-            }
+        if(count == 0){
+            Vector2 moveVector = direction * moveSpeed * Time.fixedDeltaTime;
+
+            // No collisions
+            rb.MovePosition(rb.position + moveVector);
+            return true;
         } else {
-            // Can't move if there's no direction to move in
             return false;
         }
     }
