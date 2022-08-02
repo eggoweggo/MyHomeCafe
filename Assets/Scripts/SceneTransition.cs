5053f8b33107810053475f9e5f4bf1086e94b6e0
Shannon B <89764051+eggoweggo@users.noreply.github.com>
2022-08-01 23:17:27 -0600

    fixes

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public string sceneToLoad;
+    public void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player") && !other.isTrigger){
+            SceneManager.LoadScene(sceneToLoad);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // // Start is called before the first frame update
+    // void Start()
+    // {
         
-    }
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
 }
