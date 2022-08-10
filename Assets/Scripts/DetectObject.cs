db82de7cfc0e172b18ac6828b75f6a4428f88009
Shannon B <89764051+eggoweggo@users.noreply.github.com>
2022-08-10 02:08:14 -0600

    TEXT POP UP WORKING YAYYYY

--- Assets/Scripts/DetectObject.cs
+++ Assets/Scripts/DetectObject.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class DetectObject : MonoBehaviour
 {
     [SerializeField]
-    private GameObject strawberry_text;
+    private GameObject ingredient_text;
     [SerializeField]
-    private GameObject strawberry;
+    private GameObject ingredient;
     private bool playerInRange;
     // Start is called before the first frame update
     void Start()
@@ -18,26 +18,24 @@ public class DetectObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // var strawberry_text = GameObject.Find("strawberry_text");
-        // Input.GetKey(KeyCode.E) && 
-        if(playerInRange)
-        {
-            if(strawberry_text.activeInHierarchy)
-            {
-                strawberry_text.SetActive(false);
-            } else 
-            {
-                strawberry_text.SetActive(true);
-            }
-        }
+        // // var strawberry_text = GameObject.Find("strawberry_text");
+        // // Input.GetKey(KeyCode.E) && 
+        // if(playerInRange && !ingredient_text.activeInHierarchy)
+        // {
+        //     ingredient_text.SetActive(true);
+        // } else 
+        // {
+        //     ingredient_text.SetActive(false);
+        // }
     }
-
+    
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.CompareTag("Player")) 
         {
             Debug.Log("Player in range");
             playerInRange = true;
+            ingredient_text.SetActive(true);
         }
     }
 
@@ -47,6 +45,7 @@ public class DetectObject : MonoBehaviour
         {
             Debug.Log("Player left range");
             playerInRange = false;
+            ingredient_text.SetActive(false);
         }
     }
 }
