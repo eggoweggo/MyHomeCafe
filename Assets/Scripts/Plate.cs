a882b86515ea3e2f32748cadfb8c58e5b6e3b1ae
Shannon B <89764051+eggoweggo@users.noreply.github.com>
2022-08-15 00:07:07 -0600

    Revert "plate changes when recipe is complete"
    This reverts commit 13b438772f7ebefa55e6a7e6740d9a428bd81899.

--- Assets/Scripts/Plate.cs
+++ Assets/Scripts/Plate.cs
@@ -32,7 +32,7 @@ public class Plate : MonoBehaviour
         }
         // if recipe ingredient list is empty (added all the ingredients)
         // then change plate to finished plate
-        if (currentRecipe.recipeList.Count == 0) 
+        if (currentRecipe.recipeList == null) 
         {
             plateDisplay.sprite = currentRecipe.recipeFinishedSprite;
         }
